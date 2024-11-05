b5d958c995666e82113e3bf0edbf1bc3a97863c3
Bảo Ngọc <baokhuong6996@gmail.com>
2024-11-05 22:36:40 +0700

    Finished Room

--- app/Http/Controllers/RoomController.php
+++ app/Http/Controllers/RoomController.php
@@ -118,9 +118,78 @@ class RoomController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, RoomModel $room)
     {
         //
+        $validator = Validator::make(
+            $request->all(),
+            [
+                'RoomName' => 'required|string|max:64',
+                'HotelId' => 'required|exists:Hotel,HotelId',
+                'RoomType' => 'required|string|max:30',
+                'RoomStatus' => 'required|string|max:30',
+                'Description' => 'required|string',
+                'MaxCustomer' => 'required|integer|min:1|max:3',
+                'Price' => 'required|numeric|between:0,99999999.99'
+            ],
+            [
+                // Custom thông báo lỗi cho từng trường và quy tắc
+                'RoomName.required' => 'Tên phòng không được để trống.',
+                'RoomName.string' => 'Tên phòng phải là một chuỗi ký tự.',
+                'RoomName.max' => 'Tên phòng không được dài quá 64 ký tự.',
+
+                'RoomStatus.required' => 'Trạng thái phòng không được để trống.',
+                'RoomStatus.string' => 'Trạng thái phòng phải là một chuỗi ký tự.',
+                'RoomStatus.max' => 'Trạng thái phòng không được dài quá 30 ký tự.',
+
+                'RoomType.required' => 'Kiểu phòng không được để trống.',
+                'RoomType.string' => 'Kiểu phòng phải là một chuỗi ký tự.',
+                'RoomType.max' => 'Kiểu phòng không được dài quá 30 ký tự.',
+
+                'HotelId.required' => 'ID hotel không được để trống.',
+                'HotelId.exists' => 'ID hotel không hợp lệ.',
+
+                'MaxCustomer.required' => 'Số khách tối đa không được để trống.',
+                'MaxCustomer.integer' => 'Số khách tối đa phải là số nguyên.',
+                'MaxCustomer.min' => 'Số khách tối đa phải lớn hơn hoặc bằng 1.',
+                'MaxCustomer.max' => 'Số khách tối đa phải nhỏ hơn hoặc bằng 3.',
+
+                'Price.required' => 'Giá không được để trống.',
+                'Price.numeric' => 'Giá phải là một số.',
+                'Price.between' => 'Giá phải nằm trong khoảng từ 1 đến 99999999.99.',
+            ]
+        );
+        if ($validator->fails()) {
+            return response()->json([
+                'message' => 'Sửa dữ liệu thất bại',
+                'error' => $validator->messages(),
+            ], 422);
+        }
+
+        // $existingHotel = RoomModel::where('RoomName', $request->RoomName)->exists();
+
+        // if ($existingHotel) {
+        //     return response()->json([
+        //         'message' => 'Phòng này đã tồn tại trong hệ thống.',
+        //     ], 409);
+        // }
+
+        $room->update(
+            [
+                'RoomName' => $request->RoomName,
+                'HotelId' => $request->HotelId,
+                'RoomType' => $request->RoomType,
+                'RoomStatus' => $request->RoomStatus,
+                'Description' => $request->Description,
+                'MaxCustomer' => $request->MaxCustomer,
+                'Price' => $request->Price,
+            ]
+        );
+
+        return response()->json([
+            'message' => 'Cập nhật phòng ở thành công',
+            'data' => new RoomResource($room)
+        ], 201);
     }
 
     /**
